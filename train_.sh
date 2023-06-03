5b758394cd0961780aaf6c2ee561848ce21f82b9
mattoofahad <mattoofahad@gmail.com>
2023-06-04 01:09:04 +0500

    temp

--- train_.sh
+++ train_.sh
@@ -1,3 +1,4 @@
+mkdir checkpoints
 python -m tools.jaad.train_cvae --gpu 0 --batch_size 24 --epochs 2 --dataset JAAD --model SGNet_CVAE --num_workers 0
 # python -m tools.jaad.train_cvae --gpu 0 --batch_size 24 --epochs 2 --dataset JAAD --model SGNet_CVAE --num_workers 0 --checkpoint checkpoints/model.pt
 # python -m tools.jaad.train_cvae --gpu 0 --batch_size 24 --epochs 2 --dataset JAAD --model SGNet_CVAE --num_workers 0 --checkpoint checkpoints/model.pt
